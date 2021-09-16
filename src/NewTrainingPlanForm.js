e1830ec7222b300b1c5faef8e8e3d22a67115cf7
Geoff Topley <geoff.topley@gmail.com>
2021-09-16 10:07:41 +0100

    Create form for new-training-plan-form

--- src/NewTrainingPlanForm.js
+++ src/NewTrainingPlanForm.js
@@ -1,7 +1,25 @@
 import React from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 const NewTrainingPlanForm = () => {
-  return <div>New form</div>;
+  return (
+    // controlId ensures accessibility
+    <Form style={{ marginTop: "8px", marginLeft: "8px" }}>
+      <Form.Group className="mb-3" controlId="formTextTrainingPlanTitle">
+        <Form.Label>Title</Form.Label>
+        <Form.Control type="text" placeholder="Enter plan title" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formTextTrainingPlanGoal">
+        <Form.Label>Goal</Form.Label>
+        <Form.Control type="text" placeholder="Enter goal" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
 };
 
 export default NewTrainingPlanForm;
